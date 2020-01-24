43847247a8350ab7494bce16be70c705b62ec9b6
aboxer <aboxer51@yahoo.com>
2020-01-23 20:03:20 -0500

    duplicate removal works

--- pdf_processing/rmvDups.py
+++ pdf_processing/rmvDups.py
@@ -50,6 +50,18 @@ def nameMatch(nameA,nameB):
     else:
       return False
 
+def dateMatch(dateA,dateB):
+  if dateA == None or dateB  == None: #we don't have both dates
+    return False
+  tmpA = dateA.strip()
+  tmpB = dateB.strip()
+  if len(tmpA) == 0  or len(tmpB) == 0: #we don't have both dates
+    return False
+  if tmpA == tmpB:
+    return True
+  else:
+    return False
+
 inf = open(sys.argv[1], 'r')
 r = inf.read()  #read in all the bytes into one string
 mails = json.loads(r)
@@ -64,27 +76,38 @@ for i in range(mailTot-1):
     end = i + 100
     if end > mailTot:
       end = mailTot
-    print('==================================  ',i)
-    print('mailId: ',tmp['mailId'],'date: ',tmp['date'],'from: ',tmp['from'],'to: ',tmp['to'])
     for j in range(i+1,end):
       tmp1 = mails[j]
-      if tmp['date'] == tmp1['date']: #date match is essential
-        dupIds.append(j)
-        print('--------  ',j)
-        print('mailId: ',tmp1['mailId'],'date: ',tmp1['date'],'from: ',tmp1['from'],'to: ',tmp1['to'])
+      if dateMatch(tmp['date'],tmp1['date']): #if dates match we consider it a match unless from: or to: contradict
         tmpFrom = tmp['from']
         tmp1From = tmp1['from']
         tmpTo = tmp['to']
         tmp1To = tmp1['to']
+        tmp1Id = tmp1['mailId']
         if tmpFrom != None and tmp1From != None: #we have froms to check
-          if not nameMatch(tmp1From,tmp1From):
+          if not nameMatch(tmpFrom,tmp1From):
             missCt += 1 #we got here because date was the only thing that matched, debug this
+            print('==================================  ',i)
+            print('mailId: ',tmp['mailId'],'date: ',tmp['date'],'from: ',tmp['from'],'to: ',tmp['to'])
+            print('body: ',tmp['body'])
+            print('--------  ',j)
+            print('mailId: ',tmp1['mailId'],'date: ',tmp1['date'],'from: ',tmp1['from'],'to: ',tmp1['to'])
+            print('body: ',tmp['body'])
         elif tmpTo != None and tmp1To != None: #we have tos to check
-          if not nameMatch(tmp1To,tmp1To):
+          if not nameMatch(tmpTo,tmp1To):
             missCt += 1 #we got here because date was the only thing that matched, debug this
+            print('==================================  ',i)
+            print('mailId: ',tmp['mailId'],'date: ',tmp['date'],'from: ',tmp['from'],'to: ',tmp['to'])
+            print('body: ',tmp['body'])
+            print('--------  ',j)
+            print('mailId: ',tmp1['mailId'],'date: ',tmp1['date'],'from: ',tmp1['from'],'to: ',tmp1['to'])
+            print('body: ',tmp1['body'])
 
-if not i in dupIds: #add the last email if it is not a duplicate
-  tmp = mails[i]
+        dupIds.append(j)
+
+lastId = mailTot - 1
+if not lastId  in dupIds: #add the last email if it is not a duplicate
+  tmp = mails[lastId]
   nodupMails.append(tmp)
 
 #debugging stats
