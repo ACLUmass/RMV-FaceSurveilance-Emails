f068b933d37f593a10ad454c8889d3fd96ec220a
aboxer <aboxer51@yahoo.com>
2020-04-09 17:16:02 -0400

    port basic tkinter layout

--- nlpAI/c_nlpAI.py
+++ nlpAI/c_nlpAI.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #https://sukhbinder.wordpress.com/2014/12/25/an-example-of-model-view-controller-design-pattern-with-tkinter-python/
 
-import view
+import v_nlpAI as view
 
 #create the view object first because it will be needed in callbacks
 v = view.view()
@@ -10,7 +10,10 @@ v = view.view()
 def helloCallBack():
   #put controller part of callback response here.......
   v.helloCallBack( "Hello Python", "Hello World") #view part of callback is here
-  v.buttonChg('bye')
+  #v.buttonChg('bye')
+def byeCallBack():
+  #put controller part of callback response here.......
+  v.helloCallBack( "Bye Python", "Bye World") #view part of callback is here
 
-v.setCtlBacks(helloCallBack) #give view pointers to controller callback methods
+v.setCtlBacks(helloCallBack,byeCallBack) #give view pointers to controller callback methods
 v.run() #run the tkinter loop
