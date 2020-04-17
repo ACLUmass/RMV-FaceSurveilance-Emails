7e41e3bf12798c23ac31254e6fba6a1ae4fbae0c
aboxer <aboxer51@yahoo.com>
2020-04-16 20:04:41 -0400

    restructure with control class

--- nlpAI/c_nlpAI.py
+++ nlpAI/c_nlpAI.py
@@ -1,42 +1,56 @@
 #!/usr/bin/python3
 #https://sukhbinder.wordpress.com/2014/12/25/an-example-of-model-view-controller-design-pattern-with-tkinter-python/
-
 import sys
 import v_nlpAI as view
 import m_nlpAI as model
 
+class ctl():
+  def __init__(self,view,model):
+    self.v = view
+    self.m = model
+    self.hypo = "True"
+    self.mode = "Train"
+
+  #create all the controller methods that the view object uses as callbacks
+  def hypoCback(self):
+  #put controller part of callback response here.......
+    if self.hypo == "True":
+      self.hypo = "False"
+    else:
+      self.hypo = "True"
+    self.v.hypoVback(self.hypo) #view part of callback is here
+
+  def nextCback(self):
+    (mailId,email) = self.m.getMail()
+    self.v.nextVback(mailId,email) #view part of callback is here
+
+  def prevCback(self):
+    return
+
+  def modeCback(self):
+  #put controller part of callback response here.......
+    if self.mode == "Train":
+      self.mode = "Search"
+    elif self.mode == "Search":
+      self.mode = "Read"
+    else:
+      self.mode = "Train"
+    self.v.modeVback(self.mode) #view part of callback is here
+
+  def gotoCback(self):
+    return
+
+  def runAICback(self):
+    return
+
+  def run(self):
+    self.v.hypoVback(self.hypo)
+    self.v.modeVback(self.mode)
+    self.v.setVbacks(self.hypoCback,self.nextCback,self.prevCback,self.modeCback,self.gotoCback,self.runAICback) #give view pointers to controller callback methods
+    self.v.run() #run the tkinter loop
+
 #create the view object first because it will be needed in callbacks
 v = view.view()
-hypo = "True "
-
 m = model.model(sys.argv[1])
-
-#create all the controller methods that the view object uses as callbacks
-def hypoCback():
-  global hypo  #NOTE - this belongs in model
-  #put controller part of callback response here.......
-  if hypo == "True ":
-    hypo = "False"
-  else:
-    hypo = "True "
-  v.hypoVback(hypo) #view part of callback is here
-
-def nextCback():
-  (mailId,email) = m.getMail()
-  v.nextVback(mailId,email) #view part of callback is here
-
-def prevCback():
-  return
-
-def modeCback():
-  return
-
-def gotoCback():
-  return
-
-def runAICback():
-  return
-
-v.hypoVback(hypo)
-v.setVbacks(hypoCback,nextCback,prevCback,modeCback,gotoCback,runAICback) #give view pointers to controller callback methods
-v.run() #run the tkinter loop
+c = ctl(v,m)
+c.run()
