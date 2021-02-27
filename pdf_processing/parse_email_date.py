a5fa044e5888ce1337b563c5938470a8d2114ab0
Lauren Chambers <lchambers@aclumorg>
2021-02-26 18:43:44 -0700

    Fix times and re-upload data

--- pdf_processing/parse_email_date.py
+++ pdf_processing/parse_email_date.py
@@ -39,60 +39,60 @@ def convert_date(date_string):
 		if re.search("Eastern", d):
 			# Wednesday, May 18, 2016 2:59:30 PM (UTC-05:00) Eastern Time (US & Canada)
 			d = d.split("(", 1)[0].strip()
-			dt_d = datetime.strptime(d, "%A %B %d %Y %H:%M:%S%p")
+			dt_d = datetime.strptime(d, "%A %B %d %Y %I:%M:%S%p")
 		elif (d == "") | ("·" in d) | ("Subject" in d) | (re.search("\d{4} \d \d", d) is not None) | (re.search("^\d ", d) is not None):
 			dt_d = None
 		elif re.search("day.*\d:\d\d:\d\d", d):
 			# Tuesday, December 20, 2016 3:13:54 PM
-			dt_d = datetime.strptime(d, "%A %B %d %Y %H:%M:%S%p")
+			dt_d = datetime.strptime(d, "%A %B %d %Y %I:%M:%S%p")
 		elif re.search("day.* \d?\d \d?\d:\d\d", d):
 			# Tuesday, January 31, 10:24 AM
 			dt_d = None
 		elif re.search("day\s[A-Za-z]{3}\s.*\d \d?\d:\d\d", d):
 			# Tuesday, Jun 20, 2017, 12:10 PM
-			dt_d = datetime.strptime(d, "%A %b %d %Y %H:%M%p")
+			dt_d = datetime.strptime(d, "%A %b %d %Y %I:%M%p")
 		elif re.search("day.*, \d?\d:\d\d", d):
 			# Monday, Jun 26, 2017, 7:33 PM
-			dt_d = datetime.strptime(d, "%A %B %d %Y %H:%M%p")
+			dt_d = datetime.strptime(d, "%A %B %d %Y %I:%M%p")
 		elif re.search("day.*\d \d?\d:\d\d", d):
 			# Tuesday, December 20, 2016 3:13 PM
-			dt_d = datetime.strptime(d, "%A %B %d %Y %H:%M%p")
+			dt_d = datetime.strptime(d, "%A %B %d %Y %I:%M%p")
 		elif re.search("(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s.*at.\d?\d:\d\d", d):
 			# Thu, Aug 25, 2016 at 8:32 AM
-			dt_d = datetime.strptime(d, "%a %b %d %Y at %H:%M%p")
+			dt_d = datetime.strptime(d, "%a %b %d %Y at %I:%M%p")
 		elif re.search("\d{4}.*GMT", d):
 			# 04/06/2016 9:23 AM (GMT-05:00)
 			d = d.split("(GMT")[0].strip()
-			dt_d = datetime.strptime(d, "%m/%d/%Y %H:%M%p")
+			dt_d = datetime.strptime(d, "%m/%d/%Y %I:%M%p")
 		elif re.search("GMT", d):
 			# 5/14/19 4:57 PM (GMT-05:00)
 			d = d.split("(GMT")[0].strip()
-			dt_d = datetime.strptime(d, "%m/%d/%y %H:%M%p")
+			dt_d = datetime.strptime(d, "%m/%d/%y %I:%M%p")
 		elif re.search("-\d{2}:\d{2}$", d):
 			# Wed, 11 May 2016 15:11:14 -0400
 			d = d.rsplit(" ", 1)[0]
 			dt_d = datetime.strptime(d, "%a %d %b %Y %H:%M:%S")
 		elif re.search(" \d{4} \d?\d:\d\d", d):
 			# Apr 5, 2016, 2:44 PM
-			dt_d = datetime.strptime(d, "%b %d %Y %H:%M%p")
+			dt_d = datetime.strptime(d, "%b %d %Y %I:%M%p")
 		elif re.search("day.*\d{4}.at.\d?\d:\d\d.*M", d):
 			# March 6, 2016 at 4:55:49 PM EST
 			d = re.split("[EPC][SD]T", d)[0].strip()
-			dt_d = datetime.strptime(d, "%A %B %d %Y at %H:%M%p")
+			dt_d = datetime.strptime(d, "%A %B %d %Y at %I:%M%p")
 		elif re.search("\d{4}.at.\d?\d:\d\d.*M", d):
 			# March 6, 2016 at 4:55:49 PM EST
 			d = re.split("[EPC][SD]T", d)[0].strip()
-			dt_d = datetime.strptime(d, "%B %d %Y at %H:%M:%S%p")
+			dt_d = datetime.strptime(d, "%B %d %Y at %I:%M:%S%p")
 		elif re.search("\d{4}.at.\d?\d:\d\d", d):
 			# March 24, 2016 at 16:02:13
 			d = re.split("[EPC][SD]T", d)[0].strip()
 			dt_d = datetime.strptime(d, "%B %d %Y at %H:%M:%S")
 		elif re.search("\d{1,2}/\d{1,2}/\d{4} \d{2}:\d{2}:\d{2}", d):
 			# March 24, 2016 at 16:02:13
-			dt_d = datetime.strptime(d, "%m/%d/%Y %H:%M:%S%p")
+			dt_d = datetime.strptime(d, "%m/%d/%Y %I:%M:%S%p")
 		elif re.search("\d{1,2}/\d{1,2}/\d{4} \d{2}:\d{2}", d):
 			# March 24, 2016 at 16:02:13
-			dt_d = datetime.strptime(d, "%m/%d/%Y %H:%M%p")
+			dt_d = datetime.strptime(d, "%m/%d/%Y %I:%M%p")
 		else:
 			print("couldn't match date " + d)
 			dt_d = None
@@ -107,6 +107,7 @@ def convert_date(date_string):
 # e = email_df[email_df.mailId.str.contains("msp1_155_4")]
 # print(e.date)
 # print(convert_date("5/14/19 4:57 PM (GMT-05:00)"))
+
 
 
 # for i, d in enumerate(all_dates):
