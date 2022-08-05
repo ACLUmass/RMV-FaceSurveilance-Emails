417e3a6a9b5ae9fd1532bcfbc8fd58ca32bf49e9
aboxer <aboxer51@yahoo.com>
2022-08-05 00:07:24 -0400

    added import stage of new database extraction pipeline

---
+++ mlpipe/import/code/pdf2raw.py
@@ -0,0 +1,94 @@
+import argparse
+import os
+import re
+import json
+import csv
+import pdfminer
+import pdfminer.high_level
+from pdfminer.high_level import extract_pages
+#print(pdfminer.__version__)
+
+#NOTE - I can't manipulate any of these parameters to group lines in two boxes that that are horizontally parallel
+#pdfminer.layout.LAParams(line_margin = 25.0)
+#pdfminer.layout.LAParams(word_margin = 100.0)
+#pdfminer.layout.LAParams(boxes_flow = 0.5)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('dir', help='input pdf file directory')
+parser.add_argument('grp', help='chooses group of files')
+parser.add_argument('outf', help='output text json file')
+parser.add_argument('crvf', help='output curve json file')
+parser.add_argument('dbgf', help='csv debug file')
+parser.add_argument('-f', type= int, help='number of pdf files to include')
+args = parser.parse_args()
+
+
+#input all the pdfs and put them in database order
+msp_pdfs = []
+fr_pdfs = []
+for pdf in os.listdir(args.dir):
+  if pdf.endswith('.pdf'):
+    num = re.search('\d+',pdf)
+    num = int(num.group())
+    if re.search('All MSP Emails',pdf):
+      msp_pdfs.append([num,pdf])
+    else:
+      fr_pdfs.append([num,pdf])
+
+fr_pdfs = sorted(fr_pdfs, key=lambda x: x[0])
+msp_pdfs = sorted(msp_pdfs, key=lambda x: x[0])
+tmp = [['msp' + str(x[0]),x[1]] for x in msp_pdfs]
+tmp1 = [['fr' + str(x[0]),x[1]] for x in fr_pdfs]
+if args.grp == 'fr':
+  pdfs = tmp1
+else:
+  pdfs = tmp
+
+#extract all the relevant info from each file
+text_chk = re.compile('<LTTextBoxHorizontal')
+curve_chk = re.compile('<LTCurve')
+db = []
+crvs = []
+pdf_ids = []
+
+pdf_ct = len(pdfs)
+if args.f != None:
+  pdf_ct = args.f
+
+
+for pdf in pdfs[0:pdf_ct]:
+  pdf_id = str(pdf[0])
+  pdf_ids.append(pdf_id)
+  prev_index = 0
+  pg = 0
+  for page_layout in extract_pages(args.dir + pdf[1]):
+    pg += 1
+    for el in page_layout:
+      if text_chk.match(el.__str__()) != None: 
+        if el.index <= prev_index:
+          prev_index = el.index
+        db.append([pdf_id,pg,el.x0,el.x1,el.y0,el.y1,el.get_text()])
+
+      elif curve_chk.match(el.__str__()) != None: 
+        crvs.append([pdf_id,pg,el.x0,el.x1,el.y0,el.y1,'+'])
+
+
+col_nms = 'pdf,pg,x0,x1,y0,y1,text'
+cols = col_nms.split(',') #use as first row of csv output file for column names
+col_ct = len(cols)
+cols_ex = col_nms + ' = [x for x in range(' + str(col_ct) + ')]' #executable string for input program to make variable names 
+
+tbl = [col_nms,db,pdf_ids]
+with open(args.outf,'w') as f:
+  json.dump(tbl,f,indent=2)
+
+tbl = [col_nms,crvs,pdf_ids]
+with open(args.crvf,'w') as f:
+  json.dump(tbl,f)
+        
+with open(args.dbgf, 'w', newline='') as csvfile:
+  spamwriter = csv.writer(csvfile, delimiter=',')
+  spamwriter.writerow(cols)
+  for row in db:
+    spamwriter.writerow(row)
+
---
+++ mlpipe/import/code/raw2lines.py
@@ -0,0 +1,114 @@
+import argparse
+import re
+import json
+import csv
+import itertools
+from pdfminer.high_level import extract_pages
+#print(pdfminer.__version__)
+
+
+def mklines(block):
+  pdf_id,pg,x0,x1,y0,y1,text = [x for x in range(7)]
+
+  segs = []
+  for elem in block:
+    snips = elem[text].split('\n')
+    if snips[-1] == '':
+      snips = snips[0:-1]
+    snip_ct = len(snips)
+    snip_y1 = elem[y1]
+    snip_ht = (snip_y1 - elem[y0])/snip_ct
+    snip_y0 = snip_y1 - snip_ht
+    for snip in snips:
+      segs.append([elem[pdf_id],elem[pg],elem[x0],elem[x1],snip_y0,snip_y1,snip])
+      snip_y1 = snip_y0
+      snip_y0 = snip_y1 - snip_ht
+
+  segs.sort(key=lambda row: row[y1],reverse=True)
+  lines = []
+  line = segs[0]
+  for i in range(1,len(segs)):
+    seg = segs[i]
+    seg_mid = (seg[y1] + seg[y0])/2
+    if line[y0] < seg_mid < line[y1]:
+      if seg[x0] < line[x0]: 
+        line[text] = seg[text] + ' ' + line[text]
+        line[x0] = seg[x0]
+      else:
+        line[text] = line[text] + ' ' + seg[text]
+        line[x1] = seg[x1]
+    else:
+      lines.append(line)
+      line = seg
+  lines.append(line)
+  return lines
+
+
+def mkblocks(page):
+  pdf_id,pg,x0,x1,y0,y1,text = [x for x in range(7)]
+  data = page.copy()
+
+  data.sort(key=lambda row: row[y1],reverse=True) #tops of elements from top of page down
+  data_ct = len(data)
+  blocks = []
+  block = []
+  cur_y1 = data[0][y1]
+  cur_y0 = data[0][y0]
+  for elem in data:
+    if cur_y0 > elem[y1]: #no y overlap so a whole horizontal line has been assembled 
+      blocks.extend(mklines(block))
+      block = [elem]
+      cur_y1 = elem[y1]
+      cur_y0 = elem[y0]
+    else:
+      block.append(elem)
+      if cur_y0 > elem[y0]:
+        cur_y0 = elem[y0] #extend the bottom of the block
+
+  blocks.extend(mklines(block))
+  return blocks
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('grp', help='chooses group of files')
+parser.add_argument('inf', help='input json file')
+parser.add_argument('outf', help='output text json file')
+parser.add_argument('dbgf', help='csv debug file')
+args = parser.parse_args()
+  
+with open(args.inf,'r') as inf:
+  hdr,db,pdf_ids = json.load(inf)
+
+#################### Issues with pdf to json conversion #######################
+# - Text on the same horizontal may be in different elements if the horizontal space between two words is large
+# - Text on two different horizontals will be in the same element if the space between lines is small
+# - Text on two different pages will never be in the same element
+# - Pages are skipped if empty
+# - Redactions can cause lines to be broken in separate elements
+# - The elements of a redacted paragraph can be out of order
+# - I can't fix any of this in pdfminer.six
+
+pages = []
+page = None
+pg_num = None
+pdf_id = None
+for elem in db:
+  if elem[0] != pdf_id or elem[1] != pg_num:
+    pdf_id = elem[0]
+    pg_num = elem[1]
+    if page != None:
+      pages.extend(mkblocks(page))
+    page = [elem]
+  else:
+    page.append(elem)
+pages.extend(mkblocks(page))
+
+
+
+
+with open(args.outf,'w') as f:
+  json.dump(pages,f,indent=2)
+
+
+
+exit()
