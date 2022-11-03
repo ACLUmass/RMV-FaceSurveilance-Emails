46e7239309a332f87192aed51ab9daec2074fbe6
aboxer <aboxer51@yahoo.com>
2022-11-03 13:08:42 -0400

    feature add to email lines works

--- mlpipe/add_feats/code/coptypes.py
+++ mlpipe/add_feats/code/coptypes.py
@@ -7,6 +7,8 @@ from datetime import datetime
 '''
 Parse each log entry and add features based on content. Reorganize the columns for easier manual inspection and debugging
 '''
+############################### helper functions ##################################
+
 def parseName(text):
   name = HumanName(text)
   sur_nm = name.last
@@ -28,18 +30,6 @@ def getLogNames(text):
     tmp = tmp[title.end():]
   return parseName(tmp)
 
-def getFromNames(text):
-  r = re.compile('"')
-  tmp = r.sub(' ',text)
-  p = re.match('From:',tmp)
-  tmp = tmp[p.end():]
-  q = re.search('\(|\[|\]|<|@',tmp)
-  if q != None:
-    tmp = tmp[0:q.start()]
-  s = re.compile('\.')
-  tmp = s.sub(' ',tmp)
-  return parseName(tmp)
-
 def mkTimeStamp(yr,mon,day): #mon is month abbreviation or fullname
   try:
     mon_num = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'].index(mon[0:3]) + 1
@@ -48,6 +38,7 @@ def mkTimeStamp(yr,mon,day): #mon is month abbreviation or fullname
   except:
     return None
 
+############################### main program ##################################
 
 parser = argparse.ArgumentParser()
 parser.add_argument('inf0', help='face recognition log json file')
@@ -87,117 +78,3 @@ with open(args.dbgf, 'w', newline='') as csvfile:
   for row in rq_cops:
     csvwriter.writerow(row)
  
-exit()
-  
-with open(args.inf1,'r') as inf:
-  email_db = json.load(inf)
-
-#The people sending emails includes FR requestors and MASS DOT-RMV employees 
-email_froms = [[x[6],x[8]] for x in email_db if x[7] == 'from_hdr']
-
-uniq_email_froms = []
-for email_from in email_froms:
-  first_nm,sur_nm = getFromNames(email_from[0])
-  for uniq_email_from in uniq_email_froms: 
-    if first_nm == uniq_email_from[2] and sur_nm == uniq_email_from[3]:
-      break
-  else:
-    uniq_email_froms.append([email_from[0],email_from[1],first_nm,sur_nm])
-
-     
-'''
-col_nms = 'cop,email_id,first_nm,sur_nm'
-        
-with open(args.dbgf, 'w', newline='') as csvfile:
-  csvwriter = csv.writer(csvfile, delimiter=',')
-  csvwriter.writerow(col_nms.split(','))
-  for row in uniq_email_froms:
-    csvwriter.writerow(row)
- 
-exit()
-'''
- 
-#Removing known FR requestors and MASS-DOT_RMV employees from the email senders should show us
-#the FR email requests that were omitted from the log
-not_rq_cops = []
-for uniq_email_from in uniq_email_froms:
-  for uniq_rq_cop in uniq_rq_cops:
-    if uniq_rq_cop[1] == uniq_email_from[2] and uniq_rq_cop[2] == uniq_email_from[3]:
-      break
-  else:
-    not_rq_cops.append(uniq_email_from)
-
-col_nms = 'cop,email_id,first_nm,sur_nm'
-        
-with open(args.dbgf, 'w', newline='') as csvfile:
-  csvwriter = csv.writer(csvfile, delimiter=',')
-  csvwriter.writerow(col_nms.split(','))
-  for row in not_rq_cops:
-    csvwriter.writerow(row)
-exit()
-     
-col_nms = 'cop,1st_nm,2nd_nm,agency,office,gov'
-        
-with open(args.dbgf, 'w', newline='') as csvfile:
-  csvwriter = csv.writer(csvfile, delimiter=',')
-  csvwriter.writerow(col_nms.split(','))
-  for cop,dat in rq_cops.items():
-    csvwriter.writerow([cop] + dat)
-  
-exit()
-pdf_pg = None
-pg_doc_ct = 0
-for line in db:
-  for nm,chk in line_types.items():
-    if chk.search(line[text]) != None:
-      if nm == 'from_hdr':
-        tmp = line[pdf_id] + '_' + str(line[pg])
-        if tmp != pdf_pg:
-          pdf_pg = tmp
-          pg_doc_ct = 0
-        pg_doc_ct += 1
-        doc_id = pdf_pg + '_' + str(pg_doc_ct)
-      #line.append(nm)
-      line.extend([nm,doc_id])
-      break
-  else:
-    try:
-      doc_id
-    except:
-      doc_id = None
-    #line.append(None)
-    line.extend([None,doc_id])
-
-with open(args.outf,'w') as f:
-  json.dump(db,f,indent=2)
-
-#check header completeness
-part_hdrs = []
-from_hdr_ct = 0
-saved_hdr = None
-lines_ct = 1
-for line in db:
-  if line[lntype] == 'from_hdr':
-    from_hdr_ct += 1
-    if saved_hdr != None:
-      if lines_ct < 4:
-        part_hdrs.append(saved_hdr)
-    saved_hdr = line
-    lines_ct = 1
-  elif line[lntype] == 'to_hdr': 
-    lines_ct += 1
-  elif line[lntype] == 'date_hdr': 
-    lines_ct += 1
-  elif line[lntype] == 'subj_hdr' or line[lntype] == 'rply_hdr': 
-    lines_ct += 1
-      
-col_nms = 'pdf,pg,x0,x1,y0,y1,text,lntype,doc_id'
-cols = col_nms.split(',') #use as first row of csv output file for column names
-col_ct = len(cols)
-        
-with open(args.dbgf, 'w', newline='') as csvfile:
-  spamwriter = csv.writer(csvfile, delimiter=',')
-  spamwriter.writerow(cols)
-  for row in part_hdrs:
-    spamwriter.writerow(row)
-  spamwriter.writerow(['total emails',from_hdr_ct,'missing_info',len(part_hdrs)])
--- mlpipe/add_feats/code/linetypes.py
+++ mlpipe/add_feats/code/linetypes.py
@@ -2,10 +2,67 @@ import argparse
 import re
 import json
 import csv
-from pdfminer.high_level import extract_pages
+from datetime import datetime
+from nameparser import HumanName
 #print(pdfminer.__version__)
+'''
+Parse each line and add features
+'''
 
-pdf_id,pg,x0,x1,y0,y1,text,lntype = [x for x in range(8)]
+################################### Helper Functions #################################
+def parseName(text):
+  name = HumanName(text)
+  sur_nm = name.last
+  if sur_nm == '':
+    sur_nm = name.first
+    #fst_nm = None
+    fst_nm = ''
+  else:
+    fst_nm = name.first
+  return fst_nm,sur_nm
+
+def getFromNames(text):
+  r = re.compile('"')
+  tmp = r.sub(' ',text)
+  p = re.match('From:',tmp)
+  tmp = tmp[p.end():]
+  q = re.search('\(|\[|\]|<|@',tmp)
+  if q != None:
+    tmp = tmp[0:q.start()]
+  s = re.compile('\.')
+  tmp = s.sub(' ',tmp)
+  return parseName(tmp)
+
+#def mkTimeStamp(yr,mon,day): #mon is month abbreviation or fullname
+#  try:
+#    mon_num = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'].index(mon[0:3]) + 1
+#    #return yr,mon,day
+#    return datetime(int(yr),mon_num,int(day)).timestamp()
+#  except:
+#    return None
+
+def mkTimeStamp(text):
+  parts = text.split()
+  yr = None
+  mon = None
+  day = None
+  for part in parts:
+    #tmp = re.match('\d{4}',part)
+    if re.match('\d{4}',part) != None:
+      yr = re.match('\d{4}',part).group()
+    elif re.match('\d{1,2},',part) != None:
+      day = re.match('\d{1,2},',part).group()[0:-1]
+    else:
+      try:
+        mon = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'].index(part[0:3]) + 1
+      except:
+        pass
+  if yr != None and mon != None and day != None: 
+    return datetime(int(yr),mon,int(day)).timestamp()
+  return None
+
+
+############################### main program ##################################
 line_types = {}
 line_types['from_hdr'] = re.compile('^From:')
 line_types['to_hdr'] = re.compile('^To:')
@@ -20,57 +77,52 @@ parser.add_argument('inf', help='input json file')
 parser.add_argument('outf', help='output text json file')
 parser.add_argument('dbgf', help='csv debug file')
 args = parser.parse_args()
-  
+
 with open(args.inf,'r') as inf:
   db = json.load(inf)
 
+pdf_id,pg,x0,x1,y0,y1,text,lntype = [x for x in range(8)]
 pdf_pg = None
 pg_doc_ct = 0
-for line in db:
-  for nm,chk in line_types.items():
+doc_id = None
+for line in db: #go thru each line of db
+  for nm,chk in line_types.items(): #check each line type checker to find a match
     if chk.search(line[text]) != None:
-      if nm == 'from_hdr':
-        tmp = line[pdf_id] + '_' + str(line[pg])
-        if tmp != pdf_pg:
-          pdf_pg = tmp
-          pg_doc_ct = 0
-        pg_doc_ct += 1
-        doc_id = pdf_pg + '_' + str(pg_doc_ct)
-      #line.append(nm)
-      line.extend([nm,doc_id])
       break
-  else:
-    try:
-      doc_id
-    except:
-      doc_id = None
-    #line.append(None)
+
+  if nm == 'from_hdr':
+    first_nm,sur_nm = getFromNames(line[text])
+    tmp = line[pdf_id] + '_' + str(line[pg])
+    if tmp != pdf_pg:
+      pdf_pg = tmp
+      pg_doc_ct = 0
+    pg_doc_ct += 1
+    doc_id = pdf_pg + '_' + str(pg_doc_ct)
+    line.extend([nm,doc_id,[first_nm,sur_nm]])
+  elif nm == 'date_hdr':
+    ts = mkTimeStamp(line[text]) 
+    line.extend([nm,doc_id,ts])
+  else: #no line type match, just add a doc_id if it exists
     line.extend([None,doc_id])
 
 with open(args.outf,'w') as f:
   json.dump(db,f,indent=2)
 
 #check header completeness
-part_hdrs = []
 from_hdr_ct = 0
-saved_hdr = None
-lines_ct = 1
+date_hdr_ct = 0
+part_hdrs = []
 for line in db:
   if line[lntype] == 'from_hdr':
     from_hdr_ct += 1
-    if saved_hdr != None:
-      if lines_ct < 4:
-        part_hdrs.append(saved_hdr)
-    saved_hdr = line
-    lines_ct = 1
-  elif line[lntype] == 'to_hdr': 
-    lines_ct += 1
+    part_hdrs.append([line[8],line[9],line[6]])
   elif line[lntype] == 'date_hdr': 
-    lines_ct += 1
-  elif line[lntype] == 'subj_hdr' or line[lntype] == 'rply_hdr': 
-    lines_ct += 1
+    date_hdr_ct += 1
+    part_hdrs.append([line[8],line[9],line[6]])
+	
       
-col_nms = 'pdf,pg,x0,x1,y0,y1,text,lntype,doc_id'
+#col_nms = 'pdf,pg,x0,x1,y0,y1,text,lntype,doc_id'
+col_nms = 'doc_id,feat,text'
 cols = col_nms.split(',') #use as first row of csv output file for column names
 col_ct = len(cols)
         
